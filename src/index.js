e4dca0c5a69f19ea1d8a0550db3ee8538c3aa83a
TinX <TinX@TinX.local>
2019-05-06 21:57:40 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import App from './App'; 
+
+const styles = {
+  fontFamily: 'sans-serif', 
+  textAlign: 'center'
+}
+
+ReactDOM.render(<App />, document.getElementById('root')); 
